1dc8ddbcd04be7f0d74efd31518f36e542434f51
SteveVil <tomas.vildomec@seznam.cz>
2019-10-04 17:23:31 +0200

    sdf

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,32 +6,30 @@ class AboutUs extends Component {
     return (
       <React.Fragment>
         <section className="page-section" id="about">
-          
           <div className="container">
             <div className="row no-gutters justify-content-center">
-
-            
               <div className="col-12 col-md-6 p-n3">
-               <div className="img-div"><img src={about_2} alt="" className="img-fluid img float-right"/>
-               </div>
+                <div className="img-div">
+                  <img
+                    src={about_2}
+                    alt=""
+                    className="img-fluid img float-right"
+                  />
+                </div>
               </div>
-            
+
               <div className="col-12 col-md-4">
                 <h2 className="mt-5">Radka Spěšná</h2>
-                
-                <p className="about-text">
-                  Start Bootstrap has everything you need to get your new
-                  website up and running in no time! Choose one of our open
-                  source, free to download, and easy to use themes! No strings
-                  attached!
-                </p>
-                
-              </div>
-          
-           </div> 
-          </div>
 
-       </section>
+                <p className="about-text">
+                  Sta has everything you need to get your new website up and
+                  running in no time! Choose one of our open source, free to
+                  download, and easy to use themes! No strings attached!
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </React.Fragment>
     );
   }
