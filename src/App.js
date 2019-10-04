96be650d1c08418f2539d3bec6087304afba8a47
SteveVil <tomas.vildomec@seznam.cz>
2019-10-04 07:53:49 +0200

    app
    asasas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saveddd to reload Tomas.
+          Edit <code>src/App.js</code>sdf sd fs
         </p>
         <a
           className="App-link"
