6a569ea817692e88670ab01a54d61fd112c24a21
SteveVil <tomas.vildomec@seznam.cz>
2019-10-04 22:06:18 +0200

    Updates

---
+++ precache-manifest.ec48889bf062c5be83848af743985d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e67f98908bac059046839e2fa4e63eda",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "05acb9dc9a66d4d5476d",
+    "url": "/react/static/css/main.bba2b642.chunk.css"
+  },
+  {
+    "revision": "dc5b174483bc05e78173",
+    "url": "/react/static/js/2.dd0ca3e1.chunk.js"
+  },
+  {
+    "revision": "05acb9dc9a66d4d5476d",
+    "url": "/react/static/js/main.cc4e7958.chunk.js"
+  },
+  {
+    "revision": "7cc5f1476cc57433f1ba",
+    "url": "/react/static/js/runtime-main.5db297b0.js"
+  },
+  {
+    "revision": "6b87fe356bf01ca615d5ec02b5a4d0a6",
+    "url": "/react/static/media/about_2.6b87fe35.jpg"
+  },
+  {
+    "revision": "6c1089a87f150e084f5bf19a3395d762",
+    "url": "/react/static/media/bg-masthead.6c1089a8.jpg"
+  }
+]);
